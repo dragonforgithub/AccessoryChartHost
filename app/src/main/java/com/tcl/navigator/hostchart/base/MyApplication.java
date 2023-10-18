d95f4e72a9c7cb4dde37a7c5c1b21cbba175b513
Lixialong <xlli@cn.dten.com>
2023-10-18 18:16:51 +0800

    first commit

---
+++ app/src/main/java/com/tcl/navigator/hostchart/base/MyApplication.java
@@ -0,0 +1,26 @@
+package com.tcl.navigator.hostchart.base;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.tcl.navigator.hostchart.utils.CrashHandler;
+
+/**
+ * Created by yaohui on 2017/3/3.
+ */
+
+public class MyApplication extends Application {
+
+    private static final String TAG = "HostChart";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        CrashHandler.getInstance().init(this);
+    }
+
+    public static void printLogDebug(String logString) {
+        Log.d(TAG, logString);
+    }
+}
